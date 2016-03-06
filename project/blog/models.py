17b98c7a8008c57021c853b6cfeed1dc3ae64c53
jrthorne <jthorne@magiclamp.com.au>
2016-03-06 12:13:03 +1100

    Added a poster, added templates, added css media

--- project/blog/models.py
+++ project/blog/models.py
@@ -1,26 +1,56 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.http import Http404
 
 # Create your models here.
+class Poster(models.Model):
+    # one to one with django user. You can select any user to be a player 
+    user = models.OneToOneField(User, related_name='poster')
+
+    def __str__(self):
+        return str(self.id) + ': ' + self.user.first_name + ': ' + \
+            self.user.social_auth.all()[0].provider
+
+
 class Question(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    original_poster = models.ForeignKey(User, related_name='op')
+    original_poster = models.ForeignKey(Poster, related_name='op')
 
     @property
-    def answer_max_votes(self):
+    def max_vote(self):
         maxVote = self.answers.aggregate(Max('votes'))['votes__max']
         return maxVote
+
+    @property
+    def num_ans(self):
+        numAns = self.answers.count()
+        return numAns
     
-    def __unicode__(self):
+    # remember, don't use __unicode__ if using python 3
+    def __str__(self):
         return self.title
 
+    # If the compare user is not the owner, raise 404 or return not authorised message
+    def authorized_owner_or_404(self, compareUser):
+        if compareUser != self.original_poster:
+            raise Http404
+        return
+
+
 class Answer(models.Model):
-    author = models.ForeignKey(User, related_name='author')
+    author = models.ForeignKey(Poster, related_name='author')
     question = models.ForeignKey(Question, related_name='answers')
     answer = models.TextField()
     # make votes readonly in admin and templates
     votes = models.IntegerField(default=0)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.answer[:80]
+
+    def authorized_owner_or_404(self, compareUser):
+        if compareUser != self.author:
+            raise Http404
+        return
+
+
--- project/blog/views.py
+++ project/blog/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response, get_object_or_404, redirect
+from django.core.urlresolvers import reverse_lazy
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import RequestContext
+from django.views.generic.list import ListView
+from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
+from django.forms.models import model_to_dict
+from django.utils.text import slugify
+from blog.models import Question, Answer
 
 # Create your views here.
+##################################################################
+class LoginRequiredMixin(object):
+    @classmethod
+    def as_view(cls, **initkwargs):
+        view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
+        return login_required(view)
+    
+    def get_context_data(self, **kwargs):
+        context = super(LoginRequiredMixin, self).get_context_data(**kwargs)
+        # no idea why this is required, but using the LoginRequiredMixin causes "request"
+        # not to be set in the template
+        context['request'] = self.request
+        return context
+    
+# end LoginRequiredMixin
+
+##################################################################
+class QuestionListView(LoginRequiredMixin, ListView):
+    model = Question
+    # Note that I use the default for template_name
+
+    def get_context_data(self, **kwargs):
+        context = super(QuestionListView, self).get_context_data(**kwargs)
+        # add extra context here
+        return context
+
+
+class QuestionAddView(LoginRequiredMixin, CreateView):
+    model = Question
+
+
+class QuestionModView(LoginRequiredMixin, CreateView):
+    model = Question
+
+    def get_object(self, *args, **kwargs):
+        thisQuestion = super(QuestionMod, self).get_object(*args, **kwargs)
+        thisQuestion.authorized_owner_or_404(self.request.user)
+        return thisQuestion
+
+
+class QuestionDelView(LoginRequiredMixin, DeleteView):
+    model = Question
+    success_url = reverse_lazy('questionList')        
+
+
+##################################################################
