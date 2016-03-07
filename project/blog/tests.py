5e48653a06fc6a72489909f295c03ec8610e12a9
jrthorne <jthorne@magiclamp.com.au>
2016-03-07 14:43:43 +1100

    Test redirect to login if accessing questions when unauthenticated

--- project/blog/tests.py
+++ project/blog/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+"""
+Run "./manage.py test blog" from healthblog root.
+"""
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+from .models import *
+import datetime # need to make timezone aware
+import urllib.parse
 
-# Create your tests here.
+class AuthorizationTests(TestCase):
+
+    def setUp(self):
+        # To fully test, create poster accounts here, and check that they can do things
+        # In the interests of brevity, just testing redirect to login if not logged in
+        pass
+
+    def testNoAccessWithoutLogin(self):
+        """
+        Tests that redirected to the home/login page if you are not logged in
+        """
+        response = self.client.get(reverse('questionList'), follow=True)
+        expected_url = reverse('home') + "?next=" + reverse('questionList')
+        self.assertRedirects(response, expected_url, status_code=302, 
+            target_status_code=200)
+        expected_url = reverse('home') + "?next=" + reverse('questionAdd')
+        response = self.client.get(reverse('questionAdd'), follow=True)
+        self.assertRedirects(response, expected_url, status_code=302, 
+            target_status_code=200)
